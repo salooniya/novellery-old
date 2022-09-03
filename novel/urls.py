f97d1f47d3ea263510cc3dd97014a4910e55b954
Ayush Kumar <kumarayush009@gmail.com>
2022-09-03 15:18:04 +0530

    changed Subscribe url

--- novel/urls.py
+++ novel/urls.py
@@ -6,6 +6,6 @@ urlpatterns = [
     path('<uuid:pk>',views.SpecificNovelApi.as_view()),
     # path('spotlight',views.TopTenSubscribedNovels.as_view()),
     # path('trending',views.TopTenViewedNovels.as_view()),
-    path('<uuid:pk>/subscribed',views.SubscribeNovel.as_view()),
+    path('<uuid:pk>/subscribe',views.SubscribeNovel.as_view()),
     path('',views.NovelApi.as_view()),
 ]
